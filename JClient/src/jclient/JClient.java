4d524fb1d7bc0b77395a72c22ef48147eea024fc
LucaSalvadori <45315691+LucaSalvadori@users.noreply.github.com>
2018-12-04 23:26:12 +0100

    UpdateCoreFunctions

--- JClient/src/jclient/JClient.java
+++ JClient/src/jclient/JClient.java
@@ -6,17 +6,14 @@
 package jclient;
 
 /**
- * Matteo è mona
  *
- * @author angelo.arena
+ * @author luca
  */
 import java.io.*;
 import java.net.*;
 import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import com.jcabi.xml.*;
-import javax.swing.text.Document;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -25,8 +22,6 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
-
-
 
 public class JClient {
 
@@ -46,34 +41,33 @@ public class JClient {
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out = new PrintStream(socket.getOutputStream());
 
-            
-            
             messaggio = in.readLine();//messaggio di hello
             System.out.println("Messaggio ricevuto: " + messaggio);
 
-            out.println("<name>" + name + "</name>");//send to the server the hostname
+            out.println("<root><name>" + name + "</name></root>");//send to the server the hostname
 
 //            messaggio = in.readLine(); // da eliminare
 //            System.out.println("Messaggio ricevuto: " + messaggio);
-
             Thread Tr = new Thread(new reader(in));
             Tr.start();
-            
-            
-            
-            
-            
 
             Scanner s = new Scanner(System.in);
 
-            
-
             while (true) {//aggiungere condizione di uscita
-                out.println(s.nextLine());//mando quello in input da console
+                String mess = s.nextLine();
+
+                if (mess.equals("/close")) {//comando per chiudere il client
+                    out.println("<root><close>0</close></root>");//mando il messaggio per chiudere la parte server
+                    Tr.stop();//fermo il thread che legge i messaggi (soluzione non elegante)
+                    out.close();//chiudo il resto
+                    in.close();
+                    socket.close();
+                    return;//interromo l'esecuzione (non elegante)
+                }
+
+                out.println("<root><message>" + mess + "</message></root>");//mando quello in input da console
             }
 
-//            out.close();
-//            in.close();
         } catch (IOException ex) {
             Logger.getLogger(JClient.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -91,72 +85,51 @@ public class JClient {
         public void run() {
             while (true) {
                 try {
-                    
+
                     String messaggio = in.readLine();
                     System.out.println("Messaggio ricevuto: " + messaggio);
-                    
-                    try{
-                    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-                    DocumentBuilder builder = factory.newDocumentBuilder();
-                    InputSource is = new InputSource(new StringReader(messaggio));
-                    org.w3c.dom.Document d = builder.parse(is);
-                    
-                    if(d.hasChildNodes()){
-                        NodeList childNodes = d.getChildNodes();
-                        
-                        for (int i = 0; i < childNodes.getLength(); i++) {
-                            System.out.println(childNodes.item(i).toString());
-                        }
-                        
-                        
-                        Node elementByTagName = d.getElementsByTagName("host").item(0);
-                        if(elementByTagName!=null){
-                            
-                            Element eElement = (elementByTagName.getNodeType() == Node.ELEMENT_NODE)? (Element) elementByTagName :null;
-                            
-                            NodeList elementsByTagName = eElement.getElementsByTagName("name");
+                    try {
+                        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                        DocumentBuilder builder = factory.newDocumentBuilder();
+                        InputSource is = new InputSource(new StringReader(messaggio));
+                        org.w3c.dom.Document doc = builder.parse(is);//faccio un parsing del messaggio
 
-                            for (int i = 0; i < elementsByTagName.getLength(); i++) {
-                                System.out.println(elementsByTagName.item(i).getTextContent());
+                        Element d = ((Element) doc.getElementsByTagName("root").item(0));//spachetto il tag root
+
+                        if (d.hasChildNodes()) {//se il messaggio contiene qualcosa
+                            NodeList CampiMessagio = d.getChildNodes();//estraggo i nodi
+
+                            for (int i = 0; i < CampiMessagio.getLength(); i++) {
+                                System.out.println(CampiMessagio.item(i).toString());
+                            }
+
+                            Node campiFigli = d.getElementsByTagName("host").item(0);
+                            if (campiFigli != null) {//se è presente la lista degli host
+
+                                Element eElement = (campiFigli.getNodeType() == Node.ELEMENT_NODE) ? (Element) campiFigli : null;
+
+                                NodeList ListaHost = eElement.getElementsByTagName("name");//prendo la lista degli host
+
+                                for (int i = 0; i < ListaHost.getLength(); i++) {//la scorro
+                                    System.out.println(ListaHost.item(i).getTextContent());//stampo host
+                                }
+                            }
+
+                            campiFigli = d.getElementsByTagName("message").item(0);
+                            if (campiFigli != null) {//se è presente un messaggio
+                                System.out.println(campiFigli.getTextContent());//stampo messaggio
+                                System.out.println(campiFigli.getAttributes().getNamedItem("name"));//stampo nome
+                            }
+
+                            campiFigli = d.getElementsByTagName("closing").item(0);
+                            if (campiFigli != null) {//se è presente una richiesta di chiusura
+                                //da aggiungere un metodo di chiusura
                             }
                         }
-                        
-                        
-                        elementByTagName = d.getElementsByTagName("message").item(0);
-                        if(elementByTagName!=null){
-                            System.out.println(elementByTagName.getTextContent());
-                        }
-                        
-                        
-                        
-                    }
-                    
-                    
-                    
-//                    
-//                    Node nNode = d.getElementsByTagName("host").item(0);
-//
-//                    
-                    
-                    }catch(ParserConfigurationException ex){
-                        
+                    } catch (ParserConfigurationException ex) {
                     } catch (SAXException ex) {
                         Logger.getLogger(JClient.class.getName()).log(Level.SEVERE, null, ex);
                     }
-
-
-
-                    
-
-
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
                 } catch (IOException ex) {
                     Logger.getLogger(JClient.class.getName()).log(Level.SEVERE, null, ex);
                 }
--- JServer/src/jserver/Connect.java
+++ JServer/src/jserver/Connect.java
@@ -9,88 +9,149 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.io.StringReader;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 class Connect extends Thread {
+
     ArrayList<Connect> conections;
-        private Socket client = null;
-        private BufferedReader in = null;
-        private PrintStream out = null;
-        private String host;
-        private String name;
-        private Server s;
+    private Socket client = null;
+    private BufferedReader in = null;
+    private PrintStream out = null;
+    private String host;
+    private String name = "notSet";
+    private Server s;
 
-        
-        public void print(String message){
-            out.println(message);
-        }
+    public void print(String message) {
+        out.println(message);
+    }
 
+    public String getHostName() {
+        return name;
+    }
 
-        public String getHostName() {
-            return name;
-        }
-
-        public Connect(Socket clienSocket, ArrayList<Connect> conections, Server s) {
-            this.conections = conections;
-            this.s = s;
-            client = clienSocket;
+    public Connect(Socket clienSocket, ArrayList<Connect> conections, Server s) {
+        this.conections = conections;
+        this.s = s;
+        client = clienSocket;
+        try {
+            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            out = new PrintStream(client.getOutputStream(), true);
+        } catch (IOException ex) {
             try {
-                in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                out = new PrintStream(client.getOutputStream(), true);
-            } catch (IOException ex) {
-                try {
-                    client.close();
-                } catch (Exception e) {
-                    System.err.println(e.getMessage());
-                }
-                return;
+                client.close();
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
             }
-            this.start();
+            return;
         }
+        this.start();
+    }
 
-        public void run() {
-            String message;
+    public void run() {
+        String message;
 
-            while (true) {
-                try {
-                    out.println("Hello");
-                    
-                    name = in.readLine();//leggo l'hostname
-                    
-                    message = "<host>";
-                    for (Connect conection : conections) {
-                        message += conection.getHostName();
-                    }
-                    message += "</host>";
-                    
-                    out.println(message);//mando la lista al client (implementare aggiornamenti alla lista e invio di tali aggiornamenti ai client)
-                    
-                    
-                    while(true){//aggiungere condizione di uscita
-                        message = name+"<message>"+in.readLine()+"</message>";//leggo un messaggio
-                        System.out.println(message);//stampo a video
-                        
+        while (true) {
+            try {
+                out.println("Hello");
 
-                        for (Connect conection : conections) {//lo mando a tutti i client connesi attraverso la lista delle classi conection
-                            conection.print(message);
+//                name = in.readLine();//leggo l'hostname
+//                updateHostList();
+                while (true) {//aggiungere condizione di uscita
+                    try {
+
+                        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                        DocumentBuilder builder = factory.newDocumentBuilder();
+                        InputSource is = new InputSource(new StringReader(in.readLine()));
+                        org.w3c.dom.Document doc = builder.parse(is);//faccio un parsing del messaggio
+
+                        Element d = ((Element) doc.getElementsByTagName("root").item(0));//spachetto il tag root
+
+                        if (d.hasChildNodes()) {//se il messaggio contiene qualcosa
+                            NodeList CampiMessagio = d.getChildNodes();//estraggo i nodi
+
+                            for (int i = 0; i < CampiMessagio.getLength(); i++) {
+                                System.out.println(CampiMessagio.item(i).toString());
+                            }
+
+                            Node campiFigli = d.getElementsByTagName("name").item(0);
+                            if (campiFigli != null) {//se mi manda il suo hostname
+                                name = campiFigli.getTextContent();
+                                updateHostList();
+                            }
+
+                            campiFigli = d.getElementsByTagName("message").item(0);
+                            if (campiFigli != null) {//se mi manda un messaggio
+                                message = "<root>" + "<message " + "name='" + name + "' >" + campiFigli.getTextContent() + "</message>" + "</root>";
+                                System.out.println(message);//stampo a video
+                                sendToAll(message);
+                            }
+
+                            campiFigli = d.getElementsByTagName("close").item(0);
+                            if (campiFigli != null) {//se mi manda un messaggio
+                                System.out.println("Closing conection "+name+"...");
+                                closeConection();
+                                return;
+                            }
+
                         }
-                        
+                    } catch (ParserConfigurationException ex) {
+                    } catch (SAXException ex) {
+                        Logger.getLogger(JServer.class.getName()).log(Level.SEVERE, null, ex);
                     }
-                    
-                    
+
+                }
+
 //                    out.flush(); da implementare
 //                    out.close();
 //                    in.close();
 //                    client.close();
-                } catch (IOException ex) {
-                    Logger.getLogger(JServer.class.getName()).log(Level.SEVERE, null, ex);
-                }
+            } catch (IOException ex) {
+                Logger.getLogger(JServer.class.getName()).log(Level.SEVERE, null, ex);
+                closeConection();
+                break;
             }
         }
 
     }
+
+    public void updateHostList() {
+        String message;
+        message = "<root><host>";
+        for (Connect conection : conections) {
+            message += "<name>" + conection.getHostName() + "</name>";
+        }
+        message += "</host></root>";
+        sendToAll(message);
+    }
+
+    public void sendToAll(String message) {
+        for (Connect conection : conections) {//lo mando a tutti i client connesi attraverso la lista delle classi conection
+            conection.print(message);
+        }
+    }
+
+    public synchronized void closeConection() {
+        conections.remove(this);
+        out.flush();
+        out.close();
+        try {
+            in.close();
+            client.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Connect.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
