d528e7c6fb858b91c4d7ae9ed0d36917be9ebc19
LucaSalvadori <45315691+LucaSalvadori@users.noreply.github.com>
2018-12-08 23:27:52 +0100

    ProvaJavaFX

---
+++ ProvaJavaFX/src/provajavafx/ProvaJavaFX.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provajavafx;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import org.w3c.dom.events.Event;
+
+/**
+ *
+ * @author LS_Fisso
+ */
+public class ProvaJavaFX extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+//        Button btn = new Button();
+//        btn.setText("Say 'Hello World'");
+//        btn.setOnAction(new EventHandler<ActionEvent>() {
+//            
+//            @Override
+//            public void handle(ActionEvent event) {
+//                System.out.println("Hello World!");
+//            }
+//        });
+//        
+//        StackPane root = new StackPane();
+//        root.getChildren().add(btn);
+//        
+//        Scene scene = new Scene(root, 300, 250);
+        
+       // primaryStage.setTitle("Prova");
+        
+        
+//        primaryStage.setScene(scene);
+//        primaryStage.show();
+      //  primaryStage.setScene();
+      
+      
+      Parent root = FXMLLoader.load(getClass().getResource("FXML.fxml"));
+      primaryStage.setTitle("ProvaChat");
+      primaryStage.setScene(new Scene(root,600,400));
+      primaryStage.show();
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+        
+    }
+
+   
+    
+}
